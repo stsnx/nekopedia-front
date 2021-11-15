1f822d5122f6e03e5a9ec9a48f9bef06a72cc9a7
stsnx <69145525+stsnx@users.noreply.github.com>
2021-11-16 03:44:23 +0700

    add dynamic card for response

---
+++ src/testlist.js
@@ -0,0 +1,26 @@
+export const namelis= [
+        {
+            id : 1,
+            img : 'unknownkjkj.png',
+            title : 'Kawjaw',
+            des : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempor lobortis pulvinar. Phasellus dignissim diam sit amet tincidunt feugiat. Nullam nunc nisl, rutrum sit amet consequat nec, cursus a risus. Sed viverra magna vitae felis sodales vehicula. Praesent facilisis efficitur lacinia. Nam hendrerit mi sit amet sem mollis, vitae suscipit mauris fermentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed non eros aliquam, placerat tellus a, commodo dolor."
+        },
+        {
+            id : 2,
+            img : 'unknownj.png',
+            title : 'Jill',
+            des : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempor lobortis pulvinar. Phasellus dignissim diam sit amet tincidunt feugiat. Nullam nunc nisl, rutrum sit amet consequat nec, cursus a risus. Sed viverra magna vitae felis sodales vehicula. Praesent facilisis efficitur lacinia. Nam hendrerit mi sit amet sem mollis, vitae suscipit mauris fermentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed non eros aliquam, placerat tellus a, commodo dolor."
+        },
+        {
+            id : 3,
+            img : '1615735678280.jpg',
+            title : 'Eath',
+            des : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempor lobortis pulvinar. Phasellus dignissim diam sit amet tincidunt feugiat. Nullam nunc nisl, rutrum sit amet consequat nec, cursus a risus. Sed viverra magna vitae felis sodales vehicula. Praesent facilisis efficitur lacinia. Nam hendrerit mi sit amet sem mollis, vitae suscipit mauris fermentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed non eros aliquam, placerat tellus a, commodo dolor."
+        },
+        {
+            id : 4,
+            img : 'unknownka.png',
+            title : 'Kanut',
+            des : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempor lobortis pulvinar. Phasellus dignissim diam sit amet tincidunt feugiat. Nullam nunc nisl, rutrum sit amet consequat nec, cursus a risus. Sed viverra magna vitae felis sodales vehicula. Praesent facilisis efficitur lacinia. Nam hendrerit mi sit amet sem mollis, vitae suscipit mauris fermentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed non eros aliquam, placerat tellus a, commodo dolor."
+        }
+]; 
